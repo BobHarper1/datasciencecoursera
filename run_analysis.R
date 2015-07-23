aadb567b165aaad92ba6d348d178f6b02b273e05
jmk <BobHarper1@users.noreply.github.com>
2015-07-23 12:14:43 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,16 +1,15 @@
+library(dplyr)
+
 fileURL<-"https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
 if (!file.exists("~/wearables")) { dir.create("~/wearables") ; print("new directory created") }
 
-download.file(fileURL,"./wearables/file.zip")
+setwd("~/wearables/UCI HAR dataset")
 
-## Start from here if you don't wish to download the original data (it's 60Mb)
+download.file(fileURL,"~/wearables/file.zip")
 
 unzip("~/wearables/file.zip", overwrite=TRUE, exdir="~/wearables")
 
-setwd("~/wearables/UCI HAR dataset")
-
-library(dplyr)
 
   ## 1. Merges the training and the test sets to create one data set (for x and y)
 x_test<-read.table("test/X_test.txt", sep="", colClasses="numeric")
@@ -21,27 +20,32 @@ x<-rbind(x_test,x_train)
 y<-rbind(y_test,y_train)
 
   ##  3. Uses descriptive activity names to name the activities in the data set
-y[y==1]<-"WALKING";
-  y[y==2]<-"WALKING_UPSTAIRS";
-  y[y==3]<-"WALKING_DOWNSTAIRS";
-  y[y==4]<-"SITTING";
-  y[y==5]<-"STANDING";
-  y[y==6]<-"LAYING"
-colnames(y)<-"ACTIVITY"
+y[y==1]<-"walking";
+  y[y==2]<-"upstairs";
+  y[y==3]<-"downstairs";
+  y[y==4]<-"sitting";
+  y[y==5]<-"standing";
+  y[y==6]<-"laying"
+colnames(y)<-"activity"
 
   ## 4. Appropriately labels the data set with descriptive variable names. 
 features<-read.table("features.txt")
 fvector<-features[,2]
 fvector<-as.vector(fvector)
 
+fvector<-as.character(lapply(fvector, function(v) {
+  if (is.character(v)) return(tolower(v))
+  else return(v)
+}))
+
 colnames(x)<-fvector
 
   ## Read in subject identifiers (1-30) from .txt files and append to datasets (via y)
 subject_test<-read.table("test/subject_test.txt")
-  colnames(subject_test)<-"SUBJECT"
+  colnames(subject_test)<-"subject"
 
 subject_train<-read.table("train/subject_train.txt")
-  colnames(subject_train)<-"SUBJECT"
+  colnames(subject_train)<-"subject"
 
 subjects<-rbind(subject_test,subject_train)
   y<-cbind(subjects,y)
@@ -50,10 +54,12 @@ combo<-cbind(y,x)
 
   ## 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 split1 <- combo[grep("-mean\\(\\)|-std\\(\\)", names(combo))]
-split2 <- combo[grep("SUBJECT|ACTIVITY",names(combo))]
+split1cols <- sub("\\(\\)","", colnames(split1))
+colnames(split1)<-split1cols
+split2 <- combo[grep("subject|activity",names(combo))]
 
 merged_dataset <- cbind(split2,split1)
-rm(split1,split2,y,x,subject_test,subject_train,subjects,features,combo,x_test,x_train,y_test,y_train)
+rm(split1,split2,split1cols,x,y,subject_test,subject_train,subjects,features,combo,x_test,x_train,y_test,y_train)
 
 write.table(merged_dataset, "merged_dataset.txt", row.names=FALSE)
 
@@ -61,7 +67,7 @@ write.table(merged_dataset, "merged_dataset.txt", row.names=FALSE)
  ##    with the average of each variable for each activity and each subject.
 
 summary_dataset<-tbl_df(merged_dataset) %>%
-  group_by(SUBJECT,ACTIVITY) %>%
+  group_by(subject,activity) %>%
   summarise_each(funs(mean))
 
 write.table(summary_dataset, "tidy_summary_dataset.txt", row.names=FALSE)
